225c181c543c080b5be627475e206bc27a3df637
Alkein Villajos <alkein.developments@gmail.com>
2021-07-29 06:39:01 +0800

    added order number counter logic

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,16 @@ export default {
   mounted () {
     // console.log('@VUE_APP_API_URL: ', process.env.VUE_APP_API_URL)
     this.$store.dispatch('auth/authenticate')
+
+    // this is to set the prefix of the order number
+    const prefix = this.moment().format('MM-DD-YYYY-')
+    if (!localStorage.getItem('order_date_prefix')) {
+      localStorage.setItem('order_date_prefix', prefix)
+      localStorage.setItem('order_counter', 0)
+    } else if (localStorage.getItem('order_date_prefix') !== prefix) {
+      localStorage.setItem('order_date_prefix', prefix)
+      localStorage.setItem('order_counter', 0)
+    }
   }
 }
 </script>
